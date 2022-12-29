a80618b21ce2926545f31173b13ffce384f9633b
hassan-ish <hassan.abuishaiba@gmail.com>
2022-12-30 01:05:44 +0200

    the login form validation

---
+++ src/Components/LoginForm/index.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import * as yup from "yup";
+import Button from "../Button";
+import "./style.css";
+export default class LoginForm extends Component {
+  schema = yup.object().shape({
+    email: yup.string().email().min(6).max(16).required(),
+    password: yup.string().min(8).required(),
+  });
+
+  handelSubmit = (e) => { 
+    e.preventDefault();
+    this.schema
+      .validate({
+        email: this.props.email,
+        password: this.props.password,
+      })
+      .then(() => {
+        console.log("valid");
+      })
+      .catch((e) => {
+        console.log(e.errors);
+      });
+  };
+  render() {
+    return (
+      <form className="logIn" onSubmit={this.handelSubmit}>
+        <label htmlFor="userName">Your email</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Write your email"
+          value={this.props.email}
+          onChange={this.props.handelChange}
+        />
+        <label htmlFor="password">Enter your password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="•••••••••"
+          value={this.props.password}
+          onChange={this.props.handelChange}
+        />
+        <Button text="Login" />
+      </form>
+    );
+  }
+}
