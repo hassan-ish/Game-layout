2630f1f592f0d55a9e8396c716fe899bdb578ea6
hassan-ish <hassan.abuishaiba@gmail.com>
2022-12-30 01:09:55 +0200

    use ract router for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import SignLog from "./pages/SignLog";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <SignLog/>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LogIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/> 
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
