6fb1029de0daebecdb9acae106b316228c8b694b
hassan abuishaiba <110256167+hassan-ish@users.noreply.github.com>
2023-02-13 18:12:16 +0200

    Delete src/Components/button directory

--- src/Components/button/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-import "./style.css";
-export default class Button extends Component {
-  render() {
-    return <button className="submit">{this.props.text}</button>;
-  }
-}
